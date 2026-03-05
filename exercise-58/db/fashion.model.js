8b09426c2c164e35d92605cef3f2d5f3bcb1d0a8
SkipClassGoGym <nguyenquocthinh1122@gmail.com>
2026-03-05 14:54:23 +0700

    Add exercise-58 fashion system

---
+++ exercise-58/db/fashion.model.js
@@ -0,0 +1,39 @@
+﻿const mongoose = require('mongoose');
+
+const FashionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+      maxlength: 120,
+    },
+    details: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 10,
+      maxlength: 1000,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    style: {
+      type: String,
+      required: true,
+      enum: ['Streetwear', 'Minimalist', 'Vintage'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    collection: 'Fashion',
+  }
+);
+
+module.exports = mongoose.model('Fashion', FashionSchema);
